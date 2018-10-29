9b79902b6b7cc9ee7302bcd9043d5006627a588b
ProLik <li8kang@qq.com>
2018-10-29 09:21:05 +0800

    项目初始化

---
+++ common/src/main/java/com/prolik/java/BaseUtils.java
@@ -0,0 +1,13 @@
+package com.prolik.java;
+
+public class BaseUtils {
+
+    public BaseUtils() {
+        System.out.println("common base util out!");
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("common base util out!");
+    }
+}
---
+++ customer/src/main/java/com/prolik/java/Customer.java
@@ -0,0 +1,9 @@
+package com.prolik.java;
+
+public class Customer {
+    public static void main(String[] args) {
+        BaseUtils utils = new BaseUtils();
+
+        System.out.println("custemer out");
+    }
+}
